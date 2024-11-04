7e02d9aff53c03b09777def34b50e78caaf6dbaa
Renee Carignan <rcarigna@gmail.com>
2024-11-04 14:42:47 -0500

    chore: getting tests up and running successfully

--- tests/CampaignDropdown.test.tsx
+++ tests/CampaignDropdown.test.tsx
@@ -1,21 +1,30 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 
 import { KankaContextType } from '../src/app/contexts/types';
 import CampaignDropdown from '../src/app/components/CampaignDropdown';
 import { KankaContext } from '../src/app/contexts/KankaContext';
 
-it('renders a dropdown with no data', () => {
-  const props: KankaContextType = {
-    status: 'valid',
-    error: '',
-    campaigns: [],
-    fetchData: jest.fn(),
-  };
-  render(
-    <KankaContext.Provider value={props}>
-      <CampaignDropdown />
-    </KankaContext.Provider>
-  );
-  expect(screen.getByText('No campaigns available')).toBeInTheDocument();
+describe('CampaignDropdown', () => {
+  it('renders a dropdown with no data', async () => {
+    const props: KankaContextType = {
+      status: 'valid',
+      error: '',
+      campaigns: [],
+      fetchData: jest.fn(),
+    };
+    render(
+      <KankaContext.Provider value={props}>
+        <CampaignDropdown />
+      </KankaContext.Provider>
+    );
+    expect(
+      screen.getByLabelText('Select your campaign to begin')
+    ).toBeInTheDocument();
+    await userEvent.click(
+      screen.getByLabelText('Select your campaign to begin')
+    );
+    expect(screen.getByText('No campaigns available')).toBeInTheDocument();
+  });
 });
