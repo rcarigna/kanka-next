ac835f7281c910ffb55f75f2b8c988b725d2abb0
Renee Carignan <rcarigna@gmail.com>
2024-11-06 16:43:34 -0500

    fix: updating test to account for new campaign thumbnail

--- tests/Content.test.tsx
+++ tests/Content.test.tsx
@@ -21,6 +21,22 @@ describe('Content', () => {
       error: '',
       fetchData: jest.fn(),
     },
+    campaigns: [
+      {
+        name: 'Waterdhavian Webs',
+        id: 206764,
+        locale: '',
+        entry: '',
+        entry_parsed: '',
+        image: '',
+        image_full: '',
+        image_thumb: '',
+        visibility: '',
+        visibility_id: 0,
+        created_at: '',
+        updated_at: '',
+      },
+    ],
   };
   it('renders a loading page', async () => {
     render(
@@ -66,9 +82,7 @@ describe('Content', () => {
         <Content />
       </KankaContext.Provider>
     );
-    expect(
-      screen.getByLabelText('Select your campaign to begin')
-    ).toBeInTheDocument();
+    expect(screen.getByText(props.campaigns[0].name)).toBeInTheDocument();
   });
 
   it('renders a login page', async () => {
